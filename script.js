32684e0458feda791a56198f38517652aef9f2ba
rayanrr7 <136765042+rayanrr7@users.noreply.github.com>
2024-01-23 21:47:19 +1100

    Added Number and Fill-Out Validation for function
    I used the built-in isinteger function I learnt off w3schools: (https://www.w3schools.com/jsref/jsref_isinteger.asp) to ensure all inputs are integers and are filled out

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function submitValues() {
     var stepGoalInput = document.getElementById("stepGoal");
     var calorieGoalInput = document.getElementById("calorieGoal");  
+
+    if (!stepGoalInput.value || !calorieGoalInput.value) {
+        alert("Enter both Step Goal and Calorie Goal.");
+        return;
+      }
 }
+if (!isInteger(stepGoalInput.value) || !isInteger(calorieGoalInput.value)) {
+    alert("Please enter valid integer values for Step Goal and Calorie Goal.");
+  }
