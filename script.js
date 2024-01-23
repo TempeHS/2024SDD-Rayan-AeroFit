84eaccbc6bb61ccca62e271211a6eba0bc80a189
rayanrr7 <136765042+rayanrr7@users.noreply.github.com>
2024-01-24 01:00:04 +1100

    Added localstorage system to store and retrieve variables
    I used https://www.w3schools.com/jsref/prop_win_localstorage.asp to help me store my user input from script.js in local storage so that i can retrieve it from home.html and put it inside my goals box.

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ function submitValues() {
         alert("No Empty Spaces. Only Type In Numbers");
         return;
     }
+   // Stores the users steps anf cals and then redirects it to home.html
+    localStorage.setItem("stepGoal", stepGoalValue);
+    localStorage.setItem("calGoal", calGoalValue);
 
-    alert(stepGoalValue);
+    window.location.href = "home.html";
 }
