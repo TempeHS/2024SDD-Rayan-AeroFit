8d12ca484304e0c640da5a12ffa9980035cd7091
rayanrr7 <136765042+rayanrr7@users.noreply.github.com>
2024-03-01 02:46:14 +1100

    Setup Express Webserver

--- myPWA/.database/myQuery.sql
+++ myPWA/.database/myQuery.sql
@@ -1,1 +1,5 @@
--- INSERT INTO extension(extID,name,hyperlink,about,image,language) VALUES (X,"","","","",""); -- Insert template
+--CREATE TABLE extension(extID INTEGER NOT NULL PRIMARY KEY,name TEXT NOT NULL, hyperlink TEXT NOT NULL,about TEXT NOT NULL,image TEXT NOT NULL,language TEXT NOT NULL);--
+
+INSERT INTO extension(extID,name,hyperlink,about,image,language) 
+-- VALUES (1,'Bob' ,'http://google.com' , 'Description' ,'http://imageurl.com', 'English'); 
+--SELECT * FROM extension; 
---
+++ myPWA/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+app.listen(8000, () => console.log("Server is running on Port 8000, visit http://localhost:8000/ or http://127.0.0.1:8000 to access your website") );
